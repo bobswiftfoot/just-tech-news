1a571b9f78aa68b4652f58446e1f4ddab59aaf55
Nate Pfau <natepfau@yahoo.com>
2021-07-29 21:03:02 -0500

    Added a dashboard

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) =>
@@ -81,13 +82,13 @@ router.get('/:id', (req, res) =>
         });
 });
 
-router.post('/', (req, res) =>
+router.post('/', withAuth, (req, res) =>
 {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err =>
@@ -98,7 +99,7 @@ router.post('/', (req, res) =>
 });
 
 // PUT /api/posts/upvote
-router.put('/upvote', (req, res) =>
+router.put('/upvote', withAuth, (req, res) =>
 {
     // make sure the session exists first
     if (req.session)
@@ -114,7 +115,7 @@ router.put('/upvote', (req, res) =>
     }
 });
 
-router.put('/:id', (req, res) =>
+router.put('/:id', withAuth, (req, res) =>
 {
     Post.update(
         {
@@ -142,7 +143,7 @@ router.put('/:id', (req, res) =>
         });
 });
 
-router.delete('/:id', (req, res) =>
+router.delete('/:id', withAuth, (req, res) =>
 {
     Post.destroy({
         where: {
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // GET /api/users
 router.get('/', (req, res) =>
@@ -62,7 +63,7 @@ router.get('/:id', (req, res) =>
 });
 
 // POST /api/users
-router.post('/', (req, res) =>
+router.post('/', withAuth, (req, res) =>
 {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
@@ -88,7 +89,7 @@ router.post('/', (req, res) =>
         });
 });
 
-router.post('/login', (req, res) =>
+router.post('/login', withAuth, (req, res) =>
 {
     User.findOne({
         where: {
@@ -122,7 +123,7 @@ router.post('/login', (req, res) =>
     });
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', withAuth, (req, res) => {
     if (req.session.loggedIn)
     {
         req.session.destroy(() =>
@@ -137,7 +138,7 @@ router.post('/logout', (req, res) => {
 });
 
 // PUT /api/users/1
-router.put('/:id', (req, res) =>
+router.put('/:id', withAuth, (req, res) =>
 {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
@@ -165,7 +166,7 @@ router.put('/:id', (req, res) =>
 });
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) =>
+router.delete('/:id', withAuth, (req, res) =>
 {
     User.destroy({
         where: {
